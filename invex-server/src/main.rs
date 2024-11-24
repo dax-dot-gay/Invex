8cd1aee8abcae4fe9a0501f1cbe22b4322c1332a
Dax Harris <me@dax.gay>
2024-11-24 22:38:11 +0000

    additional screaming

--- invex-server/src/main.rs
+++ invex-server/src/main.rs
@@ -3,7 +3,6 @@ use rocket::serde::json::Json;
 mod config;
 
 use config::Config;
-use sea_orm::DatabaseConnection;
 
 #[macro_use] extern crate rocket;
 
@@ -19,5 +18,5 @@ fn index(info: ServerInfo) -> Json<ServerInfo> {
 async fn rocket() -> _ {
     let rocket = rocket::build().mount("/", routes![index]);
     let conf: Config = rocket.figment().extract_inner("app").expect("App config");
-    rocket.manage::<Config>(conf.clone()).manage::<DatabaseConnection>(conf.clone().database.connect().await.expect("Unable to connect to DB"))
+    rocket.manage::<Config>(conf.clone())
 }
